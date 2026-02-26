62b0e9651bbbcb1b6b109072e8baf385e3f0c598
Retik Yadav <retiknyadav614@gmail.com>
2026-02-26 12:00:42 +0530

    fix(error): removed cors redundancy

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
-const cors = require("cors");
 
 // Load env vars
 dotenv.config();
