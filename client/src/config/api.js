8806537d90d94795d36d544808918721998b4e48
Retik Yadav <retiknyadav614@gmail.com>
2026-02-26 12:15:26 +0530

    Updated API URLs

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://lumina-1onz.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://lumina-1onz.onrender.com";
 
 export default API_URL;
